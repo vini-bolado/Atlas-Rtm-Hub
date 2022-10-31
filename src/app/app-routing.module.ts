9c6320bd1b87d2a500b47f26304dec05f434e92a
Alexandre da Silva Fernandes <afernandes@britech.global>
2022-10-31 17:39:02 -0300

    Login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilModule),
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/pages/perfil/perfil-routing.module.ts
+++ src/app/pages/perfil/perfil-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+   component: PerfilComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class LoginRoutingModule { }
+export class PerfilRoutingModule { }
